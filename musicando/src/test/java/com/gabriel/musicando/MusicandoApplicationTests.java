180bf2005112088129a5a0a710f94e72da70c02b
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-30 10:37:42 -0300

    chore: project created

---
+++ musicando/src/test/java/com/gabriel/musicando/MusicandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.musicando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
