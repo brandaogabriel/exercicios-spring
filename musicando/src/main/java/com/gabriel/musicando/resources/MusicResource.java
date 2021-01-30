e1264fca3a0913a510bb6ab177e3d788f075b7dc
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-30 17:17:43 -0300

    feat: add insert, update and delete for Music

--- musicando/src/main/java/com/gabriel/musicando/resources/MusicResource.java
+++ musicando/src/main/java/com/gabriel/musicando/resources/MusicResource.java
@@ -4,11 +4,10 @@ import com.gabriel.musicando.dtos.MusicDTO;
 import com.gabriel.musicando.services.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,25 @@ public class MusicResource {
 	public ResponseEntity<MusicDTO> findById(@PathVariable Long id) {
 		MusicDTO musicDto = this.service.findById(id);
 		return ResponseEntity.ok().body(musicDto);
+	}
+
+	@PostMapping
+	public ResponseEntity<MusicDTO> insert(@RequestBody MusicDTO musicDTO) {
+		musicDTO = this.service.insert(musicDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(musicDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(musicDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<MusicDTO> update(@PathVariable Long id, @RequestBody MusicDTO musicDTO) {
+		musicDTO = this.service.update(id, musicDTO);
+		return ResponseEntity.ok().body(musicDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		this.service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- musicando/src/main/java/com/gabriel/musicando/services/MusicService.java
+++ musicando/src/main/java/com/gabriel/musicando/services/MusicService.java
@@ -1,13 +1,19 @@
 package com.gabriel.musicando.services;
 
 import com.gabriel.musicando.dtos.MusicDTO;
+import com.gabriel.musicando.entities.Artist;
 import com.gabriel.musicando.entities.Music;
+import com.gabriel.musicando.repositories.ArtistRepository;
 import com.gabriel.musicando.repositories.MusicRepository;
+import com.gabriel.musicando.services.exceptions.DatabaseException;
 import com.gabriel.musicando.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,6 +23,9 @@ public class MusicService {
 
 	@Autowired
 	private MusicRepository repository;
+
+	@Autowired
+	private ArtistRepository artistRepository;
 
 	@Transactional(readOnly = true)
 	public List<MusicDTO> findAll() {
@@ -29,5 +38,52 @@ public class MusicService {
 		Optional<Music> obj = this.repository.findById(id);
 		Music music = obj.orElseThrow(() -> new ResourceNotFoundException("Music not found"));
 		return new MusicDTO(music, music.getAuthors(), music.getInterpreters());
+	}
+
+	@Transactional
+	public MusicDTO insert(MusicDTO musicDTO) {
+		Music music = new Music();
+		copyDtoToEntity(music, musicDTO);
+		music = this.repository.save(music);
+		return new MusicDTO(music);
+	}
+
+	@Transactional
+	public MusicDTO update(Long id, MusicDTO musicDTO) {
+		try {
+			Music music = this.repository.getOne(id);
+			copyDtoToEntity(music, musicDTO);
+			return new MusicDTO(music);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	private void copyDtoToEntity(Music music, MusicDTO musicDTO) {
+		music.setName(musicDTO.getName());
+		music.setReleaseDate(musicDTO.getReleaseDate());
+		music.setDuration(musicDTO.getDuration());
+
+		music.getAuthors().clear();
+		musicDTO.getAuthors().forEach(artistDTO -> {
+			Artist artist = this.artistRepository.getOne(artistDTO.getId());
+			music.getAuthors().add(artist);
+		});
+
+		music.getInterpreters().clear();
+		musicDTO.getInterpreters().forEach(artistDTO -> {
+			Artist artist = this.artistRepository.getOne(artistDTO.getId());
+			music.getInterpreters().add(artist);
+		});
+	}
+
+	public void delete(Long id) {
+		try {
+			this.repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integry violation");
+		}
 	}
 }
