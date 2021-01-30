000960a89de90dfb26fa11d317754931a136a3a7
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-30 15:39:11 -0300

    chore: add ResourceExceptionHandler

---
+++ musicando/src/main/java/com/gabriel/musicando/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.gabriel.musicando.resources.exceptions;
+
+import com.gabriel.musicando.services.exceptions.DatabaseException;
+import com.gabriel.musicando.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e,
+																											HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError standardError = new StandardError(Instant.now(), status.value(), "Resource not found",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> integrityViolation(DatabaseException e,
+																													HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError standardError = new StandardError(Instant.now(), status.value(), "Database " +
+				"exception",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
+}
--- musicando/src/main/java/com/gabriel/musicando/services/ArtistService.java
+++ musicando/src/main/java/com/gabriel/musicando/services/ArtistService.java
@@ -3,11 +3,15 @@ package com.gabriel.musicando.services;
 import com.gabriel.musicando.dtos.ArtistDTO;
 import com.gabriel.musicando.entities.Artist;
 import com.gabriel.musicando.repositories.ArtistRepository;
+import com.gabriel.musicando.services.exceptions.DatabaseException;
 import com.gabriel.musicando.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -40,12 +44,22 @@ public class ArtistService {
 
 	@Transactional
 	public ArtistDTO update(Long id, ArtistDTO artistDTO) {
-		Artist artist = this.repository.getOne(id);
-		artist.setName(artistDTO.getName());
-		return new ArtistDTO(artist);
+		try {
+			Artist artist = this.repository.getOne(id);
+			artist.setName(artistDTO.getName());
+			return new ArtistDTO(artist);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 
 	public void delete(Long id) {
-		this.repository.deleteById(id);
+		try {
+			this.repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integry violation");
+		}
 	}
 }
